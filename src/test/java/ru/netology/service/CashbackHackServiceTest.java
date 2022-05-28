b92067b2bebd9f692ebef0392b19608cf9b63fc6
Vladislav Ezhkov <vlad.ezhkov.96@mail.ru>
2022-05-28 08:56:38 +0300

    JUnit4 Platform commit

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -2,7 +2,7 @@ package ru.netology.service;
 
 import org.junit.Test;
 
-import static org.junit.Assert.*;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class CashbackHackServiceTest {
 
@@ -10,17 +10,17 @@ public class CashbackHackServiceTest {
     public void testRemain900() {
         CashbackHackService cashbackHackService = new CashbackHackService();
         int amount = 900;
-        int actual = cashbackHackService.remain(amount);
         int expected = 100;
-        assertEquals(actual, expected);
+        int actual = cashbackHackService.remain(amount);
+        assertEquals(expected, actual);
     }
 
     @Test
     public void testRemain1000() {
         CashbackHackService cashbackHackService = new CashbackHackService();
         int amount = 1000;
-        int actual = cashbackHackService.remain(amount);
         int expected = 0;
-        assertEquals(actual, expected);
+        int actual = cashbackHackService.remain(amount);
+        assertEquals(expected, actual);
     }
 }
